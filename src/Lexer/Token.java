e30d6be7317dff62af1367e84f317a2248e0d350
Tizio <tiziogrigio0@users.noreply.github.com>
2026-03-18 22:57:46 +0100

    Partial fix on the lexer

--- src/Lexer/Token.java
+++ src/Lexer/Token.java
@@ -22,6 +22,9 @@ public class Token {
     }
 
     public static List<Token> tokenize(String input) {
+
+        input = input.trim();
+
         List<Token> tokens = new ArrayList<>();
 
         int tokenStart = 0; // First index of the token in the string
@@ -39,56 +42,68 @@ public class Token {
             char c = input.charAt(i);
             isCharacterWhitespace = Character.isWhitespace(c);
 
+            lastType = type; // Remember the previous type
+
+            // If it's a whitespace (not already managed by the end token part of the code), ignore and go to the next character
+            if (!isCharacterWhitespace) {
+
+                // Check for known symbols
+                if (c == '=') {
+                    if (type == TokenType.UNSET) type = TokenType.EQUALS;
+                    else type = TokenType.INVALID;
+                    endTokenOnWhitespace = true;
+                }
+                else if (isValidArithmeticSymbol(c)) {
+                    if (type == TokenType.UNSET) type = TokenType.ARITHMETIC_SYMBOL;
+                    else type = TokenType.INVALID;
+                    endTokenOnWhitespace = true;
+                } // TODO make it able to get more than 1 char (<<)
+
+                // Check for numbers
+                else if (Character.isDigit(c)) {
+                    if (lastType == TokenType.UNSET) {
+                        type = TokenType.NUMBER;
+                        endTokenOnWhitespace = true;
+                    } else if (lastType != TokenType.NUMBER && lastType != TokenType.IDENTIFIER) { type = TokenType.INVALID; }
+                    // if it's already an IDENTIFIER, there is no need to set it again (numbers in the name of the variable)
+                }
+
+                // Check for identifiers
+                else if (Character.isAlphabetic(c) || isValidIdentifierChar(c) ) {
+
+                    // If it's the start of the token, set it as identifier
+                    if (lastType == TokenType.UNSET) {
+                        type = TokenType.IDENTIFIER;
+                        endTokenOnWhitespace = true;
+                    } else if (lastType != TokenType.IDENTIFIER) { type = TokenType.INVALID; }
+                }
+
+                // If none of the previous checks were right
+                else { type = TokenType.INVALID; }
+
+            } // isCharacterWhitespace() closure
+
             // Check if we reached the end of the token
             if ((type != lastType && lastType != TokenType.UNSET) || (isCharacterWhitespace && endTokenOnWhitespace)) {
                 // Create the token
                 tokens.add(
                         new Token(
-                                type,
+                                lastType,
                                 input.substring(tokenStart, i).trim(),
                                 tokenStart
                         ) // new Token()
                 ); // tokens.add()
 
                 tokenStart = i; // Set up the start of the next token
+
                 type = TokenType.UNSET; // Unset the token
                 endTokenOnWhitespace = false;
+                i--; // WORKS BUT IDK WHY, DO NOT CHANGE
             } // if closure
-
-            lastType = type; // Remember the previous type
-
-            // If it's a whitespace (not already managed by the end token part of the code), ignore and go to the next character
-            if (isCharacterWhitespace) { continue; }
-
-            // Check for known symbols
-            if (c == '=') { type = TokenType.EQUALS; }
-            else if (isValidArithmeticSymbol(c)) { type = TokenType.ARITHMETIC_SYMBOL; } // TODO make it able to get more than 1 char (<<)
-
-            // Check for numbers
-            else if (Character.isDigit(c)) {
-                // Set it as number if it's not an identifier
-                if (lastType != TokenType.IDENTIFIER) { type = TokenType.NUMBER; endTokenOnWhitespace = true; }
-                // if it's already an IDENTIFIER, there is no need to set it again (numbers in the name of the variable)
-            }
-
-            // Check for identifiers
-            else if (Character.isAlphabetic(c) || isValidIdentifierChar(c) ) {
-
-                // If it's the start of the token, set it as identifier
-                if (lastType == TokenType.UNSET) {
-                    type = TokenType.IDENTIFIER;
-                    endTokenOnWhitespace = true;
-                }
-                if (lastType == TokenType.NUMBER) { type = TokenType.INVALID; }
-            }
-
-            // If none of the previous checks were right
-            else { type = TokenType.INVALID; }
-            if (type == TokenType.INVALID) lastType = TokenType.INVALID;
 
         } // for closure
 
-        tokens.add(new Token(lastType, input.substring(tokenStart).trim(), tokenStart));
+        tokens.add(new Token(type, input.substring(tokenStart).trim(), tokenStart));
 
         return tokens;
     }
--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,21 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Token> tokens = Token.tokenize("public x7896x 1234 4 class Arma extends Item5 124 351379x");
-        int tokens_length = tokens.toArray().length;
+        TestTokenize("x = 5 + 3");
+        TestTokenize("x=5+3");
+        TestTokenize("x1 = 532 + 425");
+        TestTokenize("1x = 525x - 513x");
+        TestTokenize("x1=532+425");
 
+    }
+
+    static void TestTokenize(String s) {
+        System.out.println("\""+s+"\"");
+        List<Token> tokens = Token.tokenize(s);
+        int tokens_length = tokens.toArray().length;
         for (int i=0; i < tokens_length; i++) {
             System.out.println(tokens.get(i));
         }
-
-
+        System.out.println();
     }
 }
